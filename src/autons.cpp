8211b6bc6b2a15e5a823d06318d2d4286a61babc
SpeedSlicer <143555953+SpeedSlicer@users.noreply.github.com>
2026-01-10 20:49:01 -0500

    hello world

--- src/autons.cpp
+++ src/autons.cpp
@@ -55,272 +55,6 @@ void default_constants() {
 
   chassis.pid_angle_behavior_set(ez::shortest);  // Changes the default behavior for turning, this defaults it to the shortest path there
 }
-
-///
-// Drive Example
-///
-void drive_example() {
-  // The first parameter is target inches
-  // The second parameter is max speed the robot will drive at
-  // The third parameter is a boolean (true or false) for enabling/disabling a slew at the start of drive motions
-  // for slew, only enable it when the drive distance is greater than the slew distance + a few inches
-
-  chassis.pid_drive_set(24_in, DRIVE_SPEED, true);
-  chassis.pid_wait();
-
-  chassis.pid_drive_set(-12_in, DRIVE_SPEED);
-  chassis.pid_wait();
-
-  chassis.pid_drive_set(-12_in, DRIVE_SPEED);
-  chassis.pid_wait();
-}
-
-///
-// Turn Example
-///
-void turn_example() {
-  // The first parameter is the target in degrees
-  // The second parameter is max speed the robot will drive at
-
-  chassis.pid_turn_set(90_deg, TURN_SPEED);
-  chassis.pid_wait();
-
-  chassis.pid_turn_set(45_deg, TURN_SPEED);
-  chassis.pid_wait();
-
-  chassis.pid_turn_set(0_deg, TURN_SPEED);
-  chassis.pid_wait();
-}
-
-///
-// Combining Turn + Drive
-///
-void drive_and_turn() {
-  chassis.pid_drive_set(24_in, DRIVE_SPEED, true);
-  chassis.pid_wait();
-
-  chassis.pid_turn_set(45_deg, TURN_SPEED);
-  chassis.pid_wait();
-
-  chassis.pid_turn_set(-45_deg, TURN_SPEED);
-  chassis.pid_wait();
-
-  chassis.pid_turn_set(0_deg, TURN_SPEED);
-  chassis.pid_wait();
-
-  chassis.pid_drive_set(-24_in, DRIVE_SPEED, true);
-  chassis.pid_wait();
-}
-
-///
-// Wait Until and Changing Max Speed
-///
-void wait_until_change_speed() {
-  // pid_wait_until will wait until the robot gets to a desired position
-
-  // When the robot gets to 6 inches slowly, the robot will travel the remaining distance at full speed
-  chassis.pid_drive_set(24_in, 30, true);
-  chassis.pid_wait_until(6_in);
-  chassis.pid_speed_max_set(DRIVE_SPEED);  // After driving 6 inches at 30 speed, the robot will go the remaining distance at DRIVE_SPEED
-  chassis.pid_wait();
-
-  chassis.pid_turn_set(45_deg, TURN_SPEED);
-  chassis.pid_wait();
-
-  chassis.pid_turn_set(-45_deg, TURN_SPEED);
-  chassis.pid_wait();
-
-  chassis.pid_turn_set(0_deg, TURN_SPEED);
-  chassis.pid_wait();
-
-  // When the robot gets to -6 inches slowly, the robot will travel the remaining distance at full speed
-  chassis.pid_drive_set(-24_in, 30, true);
-  chassis.pid_wait_until(-6_in);
-  chassis.pid_speed_max_set(DRIVE_SPEED);  // After driving 6 inches at 30 speed, the robot will go the remaining distance at DRIVE_SPEED
-  chassis.pid_wait();
-}
-
-///
-// Swing Example
-///
-void swing_example() {
-  // The first parameter is ez::LEFT_SWING or ez::RIGHT_SWING
-  // The second parameter is the target in degrees
-  // The third parameter is the speed of the moving side of the drive
-  // The fourth parameter is the speed of the still side of the drive, this allows for wider arcs
-
-  chassis.pid_swing_set(ez::LEFT_SWING, 45_deg, SWING_SPEED, 45);
-  chassis.pid_wait();
-
-  chassis.pid_swing_set(ez::RIGHT_SWING, 0_deg, SWING_SPEED, 45);
-  chassis.pid_wait();
-
-  chassis.pid_swing_set(ez::RIGHT_SWING, 45_deg, SWING_SPEED, 45);
-  chassis.pid_wait();
-
-  chassis.pid_swing_set(ez::LEFT_SWING, 0_deg, SWING_SPEED, 45);
-  chassis.pid_wait();
-}
-
-///
-// Motion Chaining
-///
-void motion_chaining() {
-  // Motion chaining is where motions all try to blend together instead of individual movements.
-  // This works by exiting while the robot is still moving a little bit.
-  // To use this, replace pid_wait with pid_wait_quick_chain.
-  chassis.pid_drive_set(24_in, DRIVE_SPEED, true);
-  chassis.pid_wait();
-
-  chassis.pid_turn_set(45_deg, TURN_SPEED);
-  chassis.pid_wait_quick_chain();
-
-  chassis.pid_turn_set(-45_deg, TURN_SPEED);
-  chassis.pid_wait_quick_chain();
-
-  chassis.pid_turn_set(0_deg, TURN_SPEED);
-  chassis.pid_wait();
-
-  // Your final motion should still be a normal pid_wait
-  chassis.pid_drive_set(-24_in, DRIVE_SPEED, true);
-  chassis.pid_wait();
-}
-
-///
-// Auto that tests everything
-///
-void combining_movements() {
-  chassis.pid_drive_set(24_in, DRIVE_SPEED, true);
-  chassis.pid_wait();
-
-  chassis.pid_turn_set(45_deg, TURN_SPEED);
-  chassis.pid_wait();
-
-  chassis.pid_swing_set(ez::RIGHT_SWING, -45_deg, SWING_SPEED, 45);
-  chassis.pid_wait();
-
-  chassis.pid_turn_set(0_deg, TURN_SPEED);
-  chassis.pid_wait();
-
-  chassis.pid_drive_set(-24_in, DRIVE_SPEED, true);
-  chassis.pid_wait();
-}
-
-///
-// Interference example
-///
-void tug(int attempts) {
-  for (int i = 0; i < attempts - 1; i++) {
-    // Attempt to drive backward
-    printf("i - %i", i);
-    chassis.pid_drive_set(-12_in, 127);
-    chassis.pid_wait();
-
-    // If failsafed...
-    if (chassis.interfered) {
-      chassis.drive_sensor_reset();
-      chassis.pid_drive_set(-2_in, 20);
-      pros::delay(1000);
-    }
-    // If the robot successfully drove back, return
-    else {
-      return;
-    }
-  }
-}
-
-// If there is no interference, the robot will drive forward and turn 90 degrees.
-// If interfered, the robot will drive forward and then attempt to drive backward.
-void interfered_example() {
-  chassis.pid_drive_set(24_in, DRIVE_SPEED, true);
-  chassis.pid_wait();
-
-  if (chassis.interfered) {
-    tug(3);
-    return;
-  }
-
-  chassis.pid_turn_set(90_deg, TURN_SPEED);
-  chassis.pid_wait();
-}
-
-///
-// Odom Drive PID
-///
-void odom_drive_example() {
-  // This works the same as pid_drive_set, but it uses odom instead!
-  // You can replace pid_drive_set with pid_odom_set and your robot will
-  // have better error correction.
-
-  chassis.pid_odom_set(24_in, DRIVE_SPEED, true);
-  chassis.pid_wait();
-
-  chassis.pid_odom_set(-12_in, DRIVE_SPEED);
-  chassis.pid_wait();
-
-  chassis.pid_odom_set(-12_in, DRIVE_SPEED);
-  chassis.pid_wait();
-}
-
-///
-// Odom Pure Pursuit
-///
-void odom_pure_pursuit_example() {
-  // Drive to 0, 30 and pass through 6, 10 and 0, 20 on the way, with slew
-  chassis.pid_odom_set({{{6_in, 10_in}, fwd, DRIVE_SPEED},
-                        {{0_in, 20_in}, fwd, DRIVE_SPEED},
-                        {{0_in, 30_in}, fwd, DRIVE_SPEED}},
-                       true);
-  chassis.pid_wait();
-
-  // Drive to 0, 0 backwards
-  chassis.pid_odom_set({{0_in, 0_in}, rev, DRIVE_SPEED},
-                       true);
-  chassis.pid_wait();
-}
-
-///
-// Odom Pure Pursuit Wait Until
-///
-void odom_pure_pursuit_wait_until_example() {
-  chassis.pid_odom_set({{{0_in, 24_in}, fwd, DRIVE_SPEED},
-                        {{12_in, 24_in}, fwd, DRIVE_SPEED},
-                        {{24_in, 24_in}, fwd, DRIVE_SPEED}},
-                       true);
-  chassis.pid_wait_until_index(1);  // Waits until the robot passes 12, 24
-  // Intake.move(127);  // Set your intake to start moving once it passes through the second point in the index
-  chassis.pid_wait();
-  // Intake.move(0);  // Turn the intake off
-}
-
-///
-// Odom Boomerang
-///
-void odom_boomerang_example() {
-  chassis.pid_odom_set({{0_in, 24_in, 45_deg}, fwd, DRIVE_SPEED},
-                       true);
-  chassis.pid_wait();
-
-  chassis.pid_odom_set({{0_in, 0_in, 0_deg}, rev, DRIVE_SPEED},
-                       true);
-  chassis.pid_wait();
-}
-
-///
-// Odom Boomerang Injected Pure Pursuit
-///
-void odom_boomerang_injected_pure_pursuit_example() {
-  chassis.pid_odom_set({{{0_in, 24_in, 45_deg}, fwd, DRIVE_SPEED},
-                        {{12_in, 24_in}, fwd, DRIVE_SPEED},
-                        {{24_in, 24_in}, fwd, DRIVE_SPEED}},
-                       true);
-  chassis.pid_wait();
-
-  chassis.pid_odom_set({{0_in, 0_in, 0_deg}, rev, DRIVE_SPEED},
-                       true);
-  chassis.pid_wait();
-}
-
 ///
 // Calculate the offsets of your tracking wheels
 ///
@@ -381,29 +115,6 @@ void measure_offsets() {
   if (chassis.odom_tracker_front != nullptr) chassis.odom_tracker_front->distance_to_center_set(f_offset);
 }
 
-// . . .
-// Make your own autonomous functions here!
-// . . .
-
-void left_auton() {
-  // Example:
-  drive_example();
-}
-
-void right_auton(){
-  // Example:
-  turn_example();
-}
-
-void solo_awp() {
-  // Example:
-  combining_movements();
-}
-
-void skills_auton() {
-  // Example:
-  skillsAuton();
-}
 
 void matchload_left() {
   chassis.pid_drive_set(32_in, DRIVE_SPEED);
@@ -466,43 +177,7 @@ void kamakaze_right() {
   pros::delay(OUTTAKE_TIME * 3); // 1nce is 500ms
   intake.move(0);
 }
-void p2() {
-  // Cross over field to side opposite starting point
-  chassis.pid_drive_set(53_in, DRIVE_SPEED);
-  chassis.pid_wait();
-  chassis.pid_turn_set(-90_deg, TURN_SPEED);
-  chassis.pid_wait();
-  matchloader.set(true);
-  chassis.pid_drive_set(10_in, DRIVE_SPEED);
-  intake.move(OUTTAKE_SPEED);
-  outtake.move(OUTTAKE_SPEED);
-  pros::delay(100);
-  outtake.move(0);
-  pros::delay(OUTTAKE_TIME * 2);  
-  chassis.pid_wait();
-  intake.move(0);
-  outtake.move(0);
-  matchloader.set(false);
-  chassis.pid_drive_set(-26_in, DRIVE_SPEED);
-  chassis.pid_wait();
-  intake.move(OUTTAKE_SPEED);
-  outtake.move(OUTTAKE_SPEED);
-  pros::delay(OUTTAKE_TIME * 4);
-  intake.move(0);
-  outtake.move(0);
-}
-void parky() {
-  chassis.pid_drive_set(30_in, DRIVE_SPEED);
-  chassis.pid_wait();
-  chassis.pid_turn_set(180_deg, TURN_SPEED);
-  chassis.pid_wait();
-  chassis.pid_drive_set(65_in, 127);
-  intake.move(OUTTAKE_SPEED);
-  outtake.move(OUTTAKE_SPEED);
-  chassis.pid_wait();
-  intake.move(0);
-  outtake.move(0);
-}
+
 void skillsAuton(){
   matchload_right();  
   //escape the starting matchload
@@ -544,9 +219,39 @@ void skillsAuton(){
   intake.move(0);
   outtake.move(0);
   middlegoal.set(true);
-  p2();
-  parky();
-  
+// Cross over field to side opposite starting point
+  chassis.pid_drive_set(53_in, DRIVE_SPEED);
+  chassis.pid_wait();
+  chassis.pid_turn_set(-90_deg, TURN_SPEED);
+  chassis.pid_wait();
+  matchloader.set(true);
+  chassis.pid_drive_set(10_in, DRIVE_SPEED);
+  intake.move(OUTTAKE_SPEED);
+  outtake.move(OUTTAKE_SPEED);
+  pros::delay(100);
+  outtake.move(0);
+  pros::delay(OUTTAKE_TIME * 2);  
+  chassis.pid_wait();
+  intake.move(0);
+  outtake.move(0);
+  matchloader.set(false);
+  chassis.pid_drive_set(-26_in, DRIVE_SPEED);
+  chassis.pid_wait();
+  intake.move(OUTTAKE_SPEED);
+  outtake.move(OUTTAKE_SPEED);
+  pros::delay(OUTTAKE_TIME * 4);
+  intake.move(0);
+  outtake.move(0);
+  chassis.pid_drive_set(30_in, DRIVE_SPEED);
+  chassis.pid_wait();
+  chassis.pid_turn_set(180_deg, TURN_SPEED);
+  chassis.pid_wait();
+  chassis.pid_drive_set(65_in, 127);
+  intake.move(OUTTAKE_SPEED);
+  outtake.move(OUTTAKE_SPEED);
+  chassis.pid_wait();
+  intake.move(0);
+  outtake.move(0);  
   // chassis.pid_turn_set(-30_deg, TURN_SPEED);
   // chassis.pid_wait();
   // chassis.pid_drive_set(34_in, DRIVE_SPEED);
@@ -554,4 +259,109 @@ void skillsAuton(){
   // chassis.pid_turn_set(-60_deg, TURN_SPEED);
   // chassis.pid_wait();
   
+}
+void autonSkillsV2() {
+  // Sequence implementing original pseudo-commands using chassis and motor calls
+  // 1 Forward
+  chassis.pid_drive_set(10_in, DRIVE_SPEED); chassis.pid_wait();
+  // 2 Left 90
+  chassis.pid_turn_set(-90_deg, TURN_SPEED); chassis.pid_wait();
+  // 3 Forward
+  chassis.pid_drive_set(10_in, DRIVE_SPEED); chassis.pid_wait();
+  // 4 Left 90
+  chassis.pid_turn_set(-90_deg, TURN_SPEED); chassis.pid_wait();
+  // 5 Forward
+  chassis.pid_drive_set(10_in, DRIVE_SPEED); chassis.pid_wait();
+  // 6 Intake
+  intake.move(OUTTAKE_SPEED);
+  // 7 Backward
+  chassis.pid_drive_set(-10_in, DRIVE_SPEED); chassis.pid_wait();
+  // 8 Outtake
+  outtake.move(OUTTAKE_SPEED);
+  // 9 Forward
+  chassis.pid_drive_set(10_in, DRIVE_SPEED); chassis.pid_wait();
+  // 10 Right 90
+  chassis.pid_turn_set(90_deg, TURN_SPEED); chassis.pid_wait();
+  // 11 Forward
+  chassis.pid_drive_set(10_in, DRIVE_SPEED); chassis.pid_wait();
+  // 12 Right 90
+  chassis.pid_turn_set(90_deg, TURN_SPEED); chassis.pid_wait();
+  // 13 Forward
+  chassis.pid_drive_set(10_in, DRIVE_SPEED); chassis.pid_wait();
+  // 14 Right 90
+  chassis.pid_turn_set(90_deg, TURN_SPEED); chassis.pid_wait();
+  // 15 Forward
+  chassis.pid_drive_set(10_in, DRIVE_SPEED); chassis.pid_wait();
+  // 16 Left 90
+  chassis.pid_turn_set(-90_deg, TURN_SPEED); chassis.pid_wait();
+  // 17 Forward
+  chassis.pid_drive_set(10_in, DRIVE_SPEED); chassis.pid_wait();
+  // 18 Intake
+  intake.move(OUTTAKE_SPEED);
+  // 19 Backwards
+  chassis.pid_drive_set(-10_in, DRIVE_SPEED); chassis.pid_wait();
+  // 20 Outtake
+  outtake.move(OUTTAKE_SPEED);
+  // 21 Forward
+  chassis.pid_drive_set(10_in, DRIVE_SPEED); chassis.pid_wait();
+  // 22 Right 90
+  chassis.pid_turn_set(90_deg, TURN_SPEED); chassis.pid_wait();
+  // 23 Forward
+  chassis.pid_drive_set(10_in, DRIVE_SPEED); chassis.pid_wait();
+  // 24 Right 90
+  chassis.pid_turn_set(90_deg, TURN_SPEED); chassis.pid_wait();
+  // 25 Forward
+  chassis.pid_drive_set(10_in, DRIVE_SPEED); chassis.pid_wait();
+  // 26 Left 90
+  chassis.pid_turn_set(-90_deg, TURN_SPEED); chassis.pid_wait();
+  // 27 Forward
+  chassis.pid_drive_set(10_in, DRIVE_SPEED); chassis.pid_wait();
+  // 28 Intake
+  intake.move(OUTTAKE_SPEED);
+  // 29 Backward
+  chassis.pid_drive_set(-10_in, DRIVE_SPEED); chassis.pid_wait();
+  // 30 Outtake
+  outtake.move(OUTTAKE_SPEED);
+  // 31 Forward
+  chassis.pid_drive_set(10_in, DRIVE_SPEED); chassis.pid_wait();
+  // 32 Right 90
+  chassis.pid_turn_set(90_deg, TURN_SPEED); chassis.pid_wait();
+  // 33 Forward
+  chassis.pid_drive_set(10_in, DRIVE_SPEED); chassis.pid_wait();
+  // 34 Right 90
+  chassis.pid_turn_set(90_deg, TURN_SPEED); chassis.pid_wait();
+  // 35 Forward
+  chassis.pid_drive_set(10_in, DRIVE_SPEED); chassis.pid_wait();
+  // 36 Right 90
+  chassis.pid_turn_set(90_deg, TURN_SPEED); chassis.pid_wait();
+  // 37 Forward
+  chassis.pid_drive_set(10_in, DRIVE_SPEED); chassis.pid_wait();
+  // 38 Left 90
+  chassis.pid_turn_set(-90_deg, TURN_SPEED); chassis.pid_wait();
+  // 39 Forward
+  chassis.pid_drive_set(10_in, DRIVE_SPEED); chassis.pid_wait();
+  // 40 Intake
+  intake.move(OUTTAKE_SPEED);
+  // 41 Backward
+  chassis.pid_drive_set(-10_in, DRIVE_SPEED); chassis.pid_wait();
+  // 42 Outtake
+  outtake.move(OUTTAKE_SPEED);
+  // 43 Forward
+  chassis.pid_drive_set(10_in, DRIVE_SPEED); chassis.pid_wait();
+  // 44 Right 135
+  chassis.pid_turn_set(135_deg, TURN_SPEED); chassis.pid_wait();
+  // 45 Forward
+  chassis.pid_drive_set(10_in, DRIVE_SPEED); chassis.pid_wait();
+  // 46 Intake
+  intake.move(OUTTAKE_SPEED);
+  // 47 Outtake
+  outtake.move(OUTTAKE_SPEED);
+  // 48 Backward
+  chassis.pid_drive_set(-10_in, DRIVE_SPEED); chassis.pid_wait();
+  // 49 Left 90
+  chassis.pid_turn_set(-90_deg, TURN_SPEED); chassis.pid_wait();
+  // 50 Forward
+  chassis.pid_drive_set(10_in, DRIVE_SPEED); chassis.pid_wait();
+  // 51 Rotate 100000 -> interpret as very large turn
+  chassis.pid_turn_set(100000_deg, TURN_SPEED); chassis.pid_wait();
 }
