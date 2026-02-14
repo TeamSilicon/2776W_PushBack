a5b56f3a51d085cab029552a2942249f27eb53cc
swaritchoudhariDSTEM <68472469+SwiftyProgrammer690@users.noreply.github.com>
2026-02-14 08:18:46 -0500

    remove ez extras

--- src/main.cpp
+++ src/main.cpp
@@ -249,7 +249,7 @@ void opcontrol() {
 
   while (true) {
     // Gives you some extras to make EZ-Template ezier
-    ez_template_extras();
+    // ez_template_extras();
 
     // chassis.opcontrol_tank();  // Tank control
     chassis.opcontrol_arcade_standard(ez::SPLIT);   // Standard split arcade
